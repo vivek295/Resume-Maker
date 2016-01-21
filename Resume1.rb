12bdd5b3af7c8983ccaf2eaf57cf4ee7f79806e9
Vivek Kumar <vivek.k@mindfiresolutions.com>
2016-01-21 16:38:07 +0530

    New

--- Resume1.rb
+++ Resume1.rb
@@ -2,79 +2,71 @@ class Resume
 	attr_accessor :name,:email,:address,:mob,:qual,:skills,:about
 	class << self
 		def build
-			ob = new
-			ob.name = ob.input_name
-			ob.email = ob.input_email
-			ob.address = ob.input_address
-			ob.mob= ob.input_mob
-			ob.qual=ob.input_qual
-			ob.skills=ob.input_skills
-			ob.about=ob.input_about
-			ob.write_cv
+			resume = new
+			resume.name = resume.input_name
+			resume.email = resume.input_email
+			resume.address = resume.input_address
+			resume.mob = resume.input_mob
+			resume.qual = resume.input_qual
+			resume.skills = resume.input_skills
+			resume.about = resume.input_about
+			resume.write_cv
 		end
 	end
 
 	def input_name
 		begin
 			puts "Enter Name : "
-			name=gets
-			name.strip!
+			name = gets.strip
 		end while name.length < 2
-		return name
+		name
 	end
 
 	def input_email
 		begin
 			puts "Enter Email : "
-			email=gets
-			email.strip!
+			email = gets.strip
 		end while email.length < 5
-		return email
+		email
 	end
 
 	def input_address
 		begin
 			puts "Enter Address : "
-			address=gets
-			address.strip!
+			address = gets.strip
 		end while address.length < 5
-		return address
+		address
 	end
 
 
 	def input_mob
 		begin
 			puts "Enter Mobile number : "
-			mob=gets
-			mob.strip!
+			mob = gets.strip
 		end while mob.length != 10 or mob.to_i == 0
-		return mob
+		mob
 	end
 
 	def input_qual
 		begin
 			print "\nCourse : "
-			degree=gets
-			degree.strip!
+			degree = gets.strip
 		end while degree.length < 2
 
 		begin
 			print "Board / University : "
-			board=gets
-			board.strip!
+			board = gets.strip
 		end while board.length < 2
 
 		begin
 			print "Year of completion : "
-			yoc=gets
-			yoc.strip!
+			yoc = gets.strip
 		end while (yoc.length != 4 or yoc.to_i > 2016)
 
 		begin
 			print "Percent/CGPA : "
-			marks=gets
-			marks.strip!
-		end while marks.to_i > 100
+			marks = gets.strip
+		end while marks.to_f > 100
 
 		return "Degree : #{degree}\nBoard : #{board}\nyear of completion #{yoc}\nPer./CGPA : #{marks}"
 	end
@@ -82,22 +74,24 @@ class Resume
 	def input_skills
 		begin
 			print "Enter Skills : "
-			skills=gets.strip
+			skills = gets.strip
 		end while skills.length < 5
-		return skills
+		skills
 	end
 
 	def input_about
 		begin
 			print "About You : "
-			about=gets
-			about.strip
+			about = gets.strip
 		end while about.length < 5
+		about
 	end
 
 	def write_cv
-		myFile= File.open(self.name+".txt","w")
-		myFile.syswrite("#{name}\n#{email}\n#{address}\n#{mob}\n\n#{qual}\n\nSkills :\n#{skills}\n\nAbout : #{about}\n")
+		myFile = File.open(self.name+".txt","w")
+		data="Name : #{name}\nEmail : #{email}\nAddress : #{address}\nMobile : #{mob}\n"+
+			 "\nEducational Qualification : \n#{qual}\n\nSkills :\n#{skills}\n\nAbout : #{about}\n"
+		myFile.syswrite(data)
 	end
 end
 
